339a431b7782ac535ff2a5d596d3e09978f30c12
Natanael <natanael_magalhaes@msn.com>
2023-03-27 17:52:38 -0300

    fix basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NODE_ENV === 'production' ? '/von-neumann-machine' : ''
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath,
 }
 
 module.exports = nextConfig
