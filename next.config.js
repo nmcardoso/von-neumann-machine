386665347fe0973e0315a1d51630ec2599112648
nmcardoso <nmcardoso@users.noreply.github.com>
2023-03-27 21:15:38 +0000

    deploy: 79948f66c32a1bdd65f7fb476c8d2cb36f7340de

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const basePath = process.env.NODE_ENV === 'production' ? '/von-neumann-machine' : ''
-
-const nextConfig = {
-  reactStrictMode: true,
-  basePath,
-}
-
-module.exports = nextConfig
