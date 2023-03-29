2f8424003991a7eca86468b74c4d4e7acb148a00
nmcardoso <nmcardoso@users.noreply.github.com>
2023-03-29 20:27:21 +0000

    deploy: c0760129010898239e6ecb1f64476a96c326e1d6

---
+++ _next/static/PU7AXDMuNG-MSd4-UWvBx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/547-0b2fc09ee2f1d28e.js","static/chunks/pages/index-38a6b8d0c84f0eca.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
