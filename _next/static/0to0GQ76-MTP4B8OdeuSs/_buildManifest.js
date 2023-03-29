4a8ff85e43a0e76545986b045c65f819f2581a4b
RenatoFleury <RenatoFleury@users.noreply.github.com>
2023-03-29 20:38:03 +0000

    deploy: 7a1040c9a7e7c5b361128641d327a7b8aa345000

---
+++ _next/static/0to0GQ76-MTP4B8OdeuSs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/547-0b2fc09ee2f1d28e.js","static/chunks/pages/index-ab3b7c0bc38ae52a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
