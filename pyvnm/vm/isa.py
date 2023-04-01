d3976011422b06a0cfe149e31e8307f90ddd9f5c
Natanael <natanael_magalhaes@msn.com>
2023-04-01 02:16:32 -0300

    finalização da implementação da classe `ControlWord`

--- pyvnm/vm/isa.py
+++ pyvnm/vm/isa.py
@@ -57,7 +57,33 @@ class Word:
       return int(value, 16)
     else:
       return int(value)
+
+
+
+class ControlWord(Word):
+  MEMORY_START = 0x0
+  INSTRUCTIONS_BEGIN = 0x1
+  INSTRUCTIONS_END = 0x2
+  CODE_ENTRYPOINT = 0x3
+  
+  def __init__(self, value: str | int | None, kind: int):
+    super().__init__(value)
+    self._type = kind
     
+    
+  def __repr__(self):
+    return f'<ControlWord {self.value}>'
+  
+  
+  @property
+  def value(self) -> int | None:
+    return self._value
+  
+  
+  @value.setter
+  def value(self, new_value: str | int | None):
+    self._value = new_value    
+
 
 
 class Instruction(Word):
@@ -83,16 +109,6 @@ class Instruction(Word):
   
   
   
-class ControlInstruction(Word):
-  def __init__(self, value: str | int | None):
-    super().__init__(value)
-    
-    
-  def __repr__(self):
-    return f'<ControlInstruction {self.value}>'
-  
-  
-  
 class InstructionSet:
   JP = 0x0
   RS = 0x0
@@ -109,7 +125,6 @@ class InstructionSet:
   GD = 0xB
   PD = 0xC
   OS = 0xD
-  
   
   @classmethod
   def get_opcode(cls, symbol: str) -> int:
