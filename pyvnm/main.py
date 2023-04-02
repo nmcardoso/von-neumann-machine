5b1f2d7f5c611c84ee297254c46b465ce519bcec
Natanael <natanael_magalhaes@msn.com>
2023-04-02 05:04:10 -0300

    atualização do ponto de entrada

--- pyvnm/main.py
+++ pyvnm/main.py
@@ -1,11 +1,17 @@
+import argparse
 from pathlib import Path
 
 from vm.vnm import VonNeumannMachine
 
 
+def cli():
+  p = argparse.ArgumentParser(prog='Simulador da Maquina de Von Neumann')
+  # p.add_argument('-e', )
+
+
 def main():
   vnm = VonNeumannMachine(memory_size=128)
-  test_file = Path(__file__).parent.parent / 'test' / 'test_03.s'
+  test_file = Path(__file__).parent.parent / 'programs' / 'test_02.hex'
   vnm.execute_program(test_file)
   
 
