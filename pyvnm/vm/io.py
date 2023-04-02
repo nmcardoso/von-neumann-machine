353314a04d7591317d5038df91aaf2f1894b98ab
Natanael <natanael_magalhaes@msn.com>
2023-04-02 05:02:26 -0300

    implementação do dumper

--- pyvnm/vm/io.py
+++ pyvnm/vm/io.py
@@ -118,9 +118,34 @@ class Loader:
   
   
 class Dumper:
-  def __init__(self):
-    pass
+  def __init__(self, state: MachineState, output_path: Path = None, output_base: str = 'x'):
+    self._state = state
+    self._output_path = output_path
+    self._output_base = output_base
   
   
-  def dump(self):
-    pass
+  def dump(self) -> str:
+    first_word_addr = self._state.memory.get_first_word_address()
+    last_word_addr = self._state.memory.get_last_word_address()
+    delta = self._state.memory_start - first_word_addr
+    code_entrypoint = self._state.code_entrypoint - self._state.memory_start + delta
+    
+    bytecode = format(first_word_addr, self._output_base)
+    
+    for mem_pos in range(first_word_addr, last_word_addr + 1):
+      if mem_pos == self._state.instructions_begin:
+        bytecode += ' ['
+        
+      word = self._state.memory.read(mem_pos).value or 0x0
+      bytecode += ' ' + format(word, self._output_base)
+      
+      if mem_pos == self._state.instructions_end:
+        bytecode += ' ]'
+        
+    bytecode += format(code_entrypoint, self._output_base)
+    
+    if self._output_path is not None:
+      self._output_path.write_text(bytecode)
+    return bytecode
+    
+    
