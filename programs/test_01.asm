3ba8acffb0856c8f833ae3c86f28760bab004777
Natanael <natanael_magalhaes@msn.com>
2023-04-14 01:43:28 -0300

    atualização dos programas

---
+++ programs/test_01.asm
@@ -0,0 +1,7 @@
+        ORG     0x3
+MAIN    GD      1
+        ST      1
+        GD      1
+        AD      1
+        PD      2
+        END
---
+++ programs/test_02.asm
@@ -0,0 +1,12 @@
+        ORG     0xf
+MAIN    AD      X
+        AD      Y
+        PD      2
+        GD      0b1
+        ST      0x1
+        GD      1
+        AD      1
+        PD      2
+X       DATA    0x3
+Y       DATA    0x4
+        END
