ded51dc75c2d5e1eb34e9a1f32d819e97bf032bf
Natanael <natanael_magalhaes@msn.com>
2023-04-18 02:38:10 -0300

    remove gui

--- gui/src/components/Console.js
+++
@@ -1,9 +0,0 @@
-export default function Console() {
-  return (
-    <div
-      className="rounded bg-dark w-100 p-3"
-      style={{ height: '300px' }}>
-      <span className="text-danger">Ocorreu um erro</span>
-    </div>
-  )
-}
