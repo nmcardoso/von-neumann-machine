26efdd9dbaef0dbed723e7c5daf4dd18427401b7
Natanael <natanael_magalhaes@msn.com>
2023-04-21 22:41:53 -0300

    programs

---
+++ programs/test_os.asm
@@ -0,0 +1,4 @@
+        ORG      4    @ inicia carregamento na pos 4 da memória
+MAIN    OS       0x1  @ efetua chamada ao sistema
+        HJ       0x1  @ termina o programa
+        END
---
+++ programs/test_sc.asm
@@ -0,0 +1,18 @@
+        ORG     4       @ inicia carregamento na pos 4 da memória
+MAIN    SC      FUNC_A  @ chama função A
+        HJ      0x1     @ termina o programa
+
+FUNC_A  
+        SC      FUNC_B  @ chama função B
+        LD      A       @ carrega valor 10 no acc
+        PD      2       @ imprime valor 10
+        RS      FUNC_A
+
+FUNC_B  
+        LD      B       @ carrega valor 20 no acc
+        PD      2       @ imprime valor 20
+        RS      FUNC_B
+
+A       DATA    10
+B       DATA    20
+        END
