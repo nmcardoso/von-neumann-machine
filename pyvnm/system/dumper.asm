7790757bf82ff0fdf1552fc6448b84536035198f
Natanael <natanael_magalhaes@msn.com>
2023-07-16 02:14:15 -0300

    dumper

--- pyvnm/system/dumper.asm
+++ pyvnm/system/dumper.asm
@@ -1,17 +1,17 @@
-        ORG     208             @ carrega dumper após o loader
+        ORG     0xf0            @ carrega dumper após o loader
 MAIN    LD      MEM_START       @ carrega posição inicial proveniente do loader
         SC      CREATE_LD       @ cria instrução LD para ler a posição inicial
         SC      EXEC_LD         @ executa a instrução LD
         SC      INC_CHECKSUM    @ incrementa checksum com os 2 bytes da posição
         SC      WRITE_BYTES     @ grava os bytes da posição inicial no disco
-        LD      180             @ carrega número de bytes proveniente do loader
+        LD      0xc6            @ carrega número de bytes proveniente do loader
         SC      INC_CHECKSUM    @ incrementa checksum com o número de bytes
         PD      0x4             @ grava o byte menos signif. de N_BYES no disco
         SB      NUM_2           @ subtrai os 2 bytes de endereço
         SB      NUM_1           @ subtrai 1 byte de N_BYTES
         SB      NUM_1           @ subtrai 1 byte correspondente ao checksum
         ST      REMAINING_BYTES @ salva no contador de bytes restantes
-        LD      182             @ carrega posição inicial proveninete do loader
+        LD      0xc8            @ carrega posição inicial proveninete do loader
         SC      CREATE_LD       @ cria função LD apontando para posição inicial
 LOOP    SC      EXEC_LD         @ carrega conteúdo da posição de memória apontada
         SC      WRITE_BYTES     @ inicia o loop de escrita no disco
@@ -55,8 +55,8 @@ INC_CHECKSUM                    @ subrotina: inc. checksum sem causar side-effec
         ST      CHECKSUM        @ armazena novo valor do checksum
         LD      TMP             @ recarrega valor inicial do acc (evita SE)
         RS      INC_CHECKSUM
-MEM_START       DATA    182     @ endereço onde o loader armazena a posição inicial do programa carregado
-N_BYTES         DATA    180     @ endereço onde o loader armazena a quantidade de bytes do programa
+MEM_START       DATA    0xc6    @ endereço onde o loader armazena a posição inicial do programa carregado
+N_BYTES         DATA    0xc8    @ endereço onde o loader armazena a quantidade de bytes do programa
 REMAINING_BYTES DATA    0       @ contador de bytes restantes
 CHECKSUM        DATA    0       @ acumulador checksum
 CHECKSUM_BYTE   DATA    0       @ byte menos significativo do checksum
